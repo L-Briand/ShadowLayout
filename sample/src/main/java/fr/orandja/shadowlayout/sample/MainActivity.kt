d7288cf732f1560dda0837a6db3554424afaf6aa
Lionel Briand <lionel.briand@orandja.fr>
2020-06-26 07:48:25 +0200

    add animated radius on multicolor layout

--- sample/src/main/java/fr/orandja/shadowlayout/sample/MainActivity.kt
+++ sample/src/main/java/fr/orandja/shadowlayout/sample/MainActivity.kt
@@ -1,11 +1,21 @@
 package fr.orandja.shadowlayout.sample
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ValueAnimator.ofFloat(0f, 0f, 25f, 25f).apply {
+            duration = 2000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+            addUpdateListener { __rainbow.radius = it.animatedValue as Float }
+        }.start()
     }
 }
