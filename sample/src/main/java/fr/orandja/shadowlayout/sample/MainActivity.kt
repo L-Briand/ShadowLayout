aa22114cd87a54b55759f5d9e897f672cce2c2db
Lionel Briand <lionel.briand@orandja.fr>
2020-06-26 07:04:04 +0200

    add sample project

---
+++ sample/src/main/java/fr/orandja/shadowlayout/sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.orandja.shadowlayout.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
